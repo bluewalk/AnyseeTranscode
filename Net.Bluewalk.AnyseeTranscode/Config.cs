204dce72411b2f5c2a2032b17c0377c7a4592902
bluewalk <noreply@bluewalk.net>
2020-06-03 14:57:07 +0200

    Initial commit

---
+++ Net.Bluewalk.AnyseeTranscode/Config.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualBasic.CompilerServices;
+using Net.Bluewalk.DotNetEnvironmentExtensions;
+
+namespace Net.Bluewalk.AnyseeTranscode
+{
+    public class Config
+    {
+        [EnvironmentVariable(Name = "ANYSEE_URL", Default = "http://localhost/chlist/")]
+        public string AnyseeUrl { get; set; }
+
+        [EnvironmentVariable(Name = "HTTP_PORT", Default = 8080)]
+        public int HttpPort { get; set; }
+
+        [EnvironmentVariable(Name = "URL_PREFIX", Default = "http://localhost")]
+        public string UrlPrefix { get; set; }
+
+        [EnvironmentVariable(Name = "FFMPEG_EXE",  Default = "ffmpeg")]
+        public string FfmpegExe { get; set; }
+
+        [EnvironmentVariable(Name = "SEGMENT_PATH", Default = "/tmp")]
+        public string SegmentPath { get; set; }
+    }
+}
