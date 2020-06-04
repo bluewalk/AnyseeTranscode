2c962d8c22f009bf3ad56ecb154e77052278dd2e
Délano <delano.reijnierse@zrefinery.nl>
2020-06-04 11:44:16 +0200

    Fixed some minor things

--- Net.Bluewalk.AnyseeTranscode/Config.cs
+++ Net.Bluewalk.AnyseeTranscode/Config.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.VisualBasic.CompilerServices;
-using Net.Bluewalk.DotNetEnvironmentExtensions;
+﻿using Net.Bluewalk.DotNetEnvironmentExtensions;
 
 namespace Net.Bluewalk.AnyseeTranscode
 {
     public class Config
     {
-        [EnvironmentVariable(Name = "ANYSEE_URL", Default = "http://localhost/chlist/")]
-        public string AnyseeUrl { get; set; }
+        [EnvironmentVariable(Name = "ANYSEE_IP", Default = "192.168.1.10")]
+        public string AnyseeIp { get; set; }
 
         [EnvironmentVariable(Name = "HTTP_PORT", Default = 8080)]
         public int HttpPort { get; set; }
